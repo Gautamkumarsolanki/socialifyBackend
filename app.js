960a2560b6ed5fecd37fc1b57a6e81279377c798
Gautamkumarsolanki <42197609+Gautamkumarsolanki@users.noreply.github.com>
2024-03-04 15:18:48 +0530

    adding origin

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000','https://socialify-frontend.onrender.com'],
     credentials: true
 }));
 require('./models/model')
